1453ebe5747dd425ce9aeaafee766fc46ea3e8e6
Jean Grandmaire <grandmaire@gmail.com>
2018-10-23 02:25:25 +0000

    Copy uber code

--- src/views/UberWizard.vue
+++ src/views/UberWizard.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1>Claim Uber Code</h1>
-    <p> Enter the following promo code under Promotions in the Payments tab of your Uber app to claim your ride promotion: <em>eqw5jyq</em> </p>
+    <p> Enter the following promo code under Promotions in the Payments tab of your Uber app to claim your ride promotion: <em>{{uberCode}}</em> [<a @click.stop.prevent="copyUberCode">copy code</a>] </p>
     <v-stepper ref="stepper" :steps="steps" v-model="step"></v-stepper>
 
+    <input type="hidden" id="uber-code" :value="uberCode">
     <template v-if="step === 1">
         <ul>
-            <li>Open you <span class="android">Android</span> or <span class="ios">iOS</span> <em>Uber</em> app</li>
+            <li>Open your <span class="android-color">Android</span> or <span class="ios-color">iOS </span> <em>Uber</em> app</li>
             <li>Select the hamburger icon in the top left corner</li>
             <li>Select "Payment"</li>
         </ul>
@@ -24,7 +25,7 @@
     </template>
     <template v-if="step === 3">
         <ul>
-            <li>Enter promo code: <em>eqw5jyq</em></li>
+            <li>Enter or paste promo code: <em>eqw5jyq</em> [<a @click.stop.prevent="copyUberCode">copy code</a>]</li>
         </ul>
         <img class="screenshot android" src="../assets/android3.png"/>
         <img class="screenshot ios" src="../assets/iOS3.jpg"/>
@@ -44,12 +45,32 @@ import { VStepper } from "vue-stepper-component";
 
 @Component({
   components: {
-    VStepper,
-  },
+    VStepper
+  }
 })
 export default class UberWiz extends Vue {
   private steps: number = 3;
   private step: number = 1;
+  private uberCode: string = "eqw5jyq";
+
+  copyUberCode() {
+    let testingCodeToCopy = document.querySelector("#uber-code");
+    if (testingCodeToCopy != null) {
+      testingCodeToCopy.setAttribute("type", "text");
+      testingCodeToCopy.select();
+    }
+    try {
+      var successful = document.execCommand("copy");
+    } catch (err) {
+      alert("Oops, unable to copy");
+    }
+
+    /* unselect the range */
+    if (testingCodeToCopy != null) {
+      testingCodeToCopy.setAttribute("type", "hidden");
+      window.getSelection().removeAllRanges();
+    }
+  }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -64,37 +85,50 @@ li {
 }
 
 .button {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
 }
 
 .buttonContainer {
-    margin: 15px 0;
+  margin: 15px 0;
 }
 
 .android {
-    border-color: #4CAF50;
-    border-style: solid;
-    border-width: 5px;
+  border-color: #4caf50;
+  border-style: solid;
+  border-width: 5px;
+}
+
+.android-color {
+  color: #4caf50;
 }
 
 .ios {
-    border-color: rgb(76, 119, 175);
-    border-style: solid;
-    border-width: 5px;
+  border-color: rgb(76, 119, 175);
+  border-style: solid;
+  border-width: 5px;
+}
+
+.ios-color {
+  color: rgb(76, 119, 175);
 }
 
 em {
-    font-weight: bold;
-    font-size: 1.2em;
-    font-style: normal;
+  font-weight: bold;
+  font-size: 1.2em;
+  font-style: normal;
+}
+
+a {
+  text-decoration: underline;
+  font-size: 1.2em;
 }
 </style>
